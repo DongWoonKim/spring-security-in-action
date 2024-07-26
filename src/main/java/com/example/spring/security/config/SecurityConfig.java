9f6cd6d9735c3b3dd6856923118a36051bebe36b
DongWoonKim <01030985843z@naver.com>
2024-07-26 16:59:05 +0900

    UserDetailsManager 계약 구현

--- src/main/java/com/example/spring/security/config/SecurityConfig.java
+++ src/main/java/com/example/spring/security/config/SecurityConfig.java
@@ -1,17 +1,15 @@
 package com.example.spring.security.config;
 
-import com.example.spring.security.model.User;
-import com.example.spring.security.service.InMemoryUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
-import java.util.List;
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
@@ -34,11 +32,26 @@ public class SecurityConfig {
 //        return http.build();
 //    }
 
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        UserDetails u = new User("john", "12345", "read");
+//        List<UserDetails> users = List.of(u);
+//        return new InMemoryUserDetailsService(users);
+//    }
+
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails u = new User("john", "12345", "read");
-        List<UserDetails> users = List.of(u);
-        return new InMemoryUserDetailsService(users);
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+
+        String usersByUsernameQuery = "select username, password, enabled from spring_security.users where username = ?";
+        String authsByUserQuery = "select username, authority from spring_security.authorities where username = ?";
+
+        var userDetailsManager = new JdbcUserDetailsManager(dataSource);
+        userDetailsManager.setUsersByUsernameQuery(usersByUsernameQuery);
+        userDetailsManager.setAuthoritiesByUsernameQuery(authsByUserQuery);
+
+        return userDetailsManager;
+
+//        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
