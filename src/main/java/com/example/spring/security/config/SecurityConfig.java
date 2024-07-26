acda028d8e1827da8ead00ec5275d570198a6271
DongWoonKim <01030985843z@naver.com>
2024-07-26 11:52:41 +0900

    엔드포인트 권한 부여 구성 재정의

--- src/main/java/com/example/spring/security/config/SecurityConfig.java
+++ src/main/java/com/example/spring/security/config/SecurityConfig.java
@@ -2,14 +2,19 @@ package com.example.spring.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
@@ -33,6 +38,18 @@ public class SecurityConfig {
 
         // 또는 비밀번호 암호화를 사용하지 않으려면 다음을 사용.
          return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .anyRequest().permitAll()
+                )
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
     }
 
 }
