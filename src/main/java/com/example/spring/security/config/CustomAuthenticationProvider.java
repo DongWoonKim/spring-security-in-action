cf164bab79844652b90cbb38dcd9f0e0c7aec8ec
DongWoonKim <01030985843z@naver.com>
2024-07-26 12:21:15 +0900

    AuthenticationProvider 구현 재정의

--- src/main/java/com/example/spring/security/config/CustomAuthenticationProvider.java
+++ src/main/java/com/example/spring/security/config/CustomAuthenticationProvider.java
@@ -1,2 +1,35 @@
-package com.example.spring.security.config;public class CustomAuthenticationProvider {
+package com.example.spring.security.config;
+
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+
+        // 인증 논리를 추가할 위치
+        String username = authentication.getName();
+        String password = String.valueOf(authentication.getCredentials());
+
+        if ("john".equals(username) && "12345".equals(password)) {
+            return new UsernamePasswordAuthenticationToken(username, password, Arrays.asList());
+        } else {
+            throw new AuthenticationCredentialsNotFoundException("Error!");
+        }
+
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        // Authentication 형식의 구현을 추가할 위치
+        return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
+    }
 }
--- src/main/java/com/example/spring/security/config/SecurityConfig.java
+++ src/main/java/com/example/spring/security/config/SecurityConfig.java
@@ -1,53 +1,32 @@
 package com.example.spring.security.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        var userDetailService = new InMemoryUserDetailsManager();
-
-        var user = User.withUsername("john")
-                .password("12345")
-                .roles("USER")
-                .build();
-
-        userDetailService.createUser( user );
-
-        return userDetailService;
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        // BCryptPasswordEncoder를 사용하는 경우
-//        return new BCryptPasswordEncoder();
-
-        // 또는 비밀번호 암호화를 사용하지 않으려면 다음을 사용.
-         return NoOpPasswordEncoder.getInstance();
-    }
+    private final CustomAuthenticationProvider authenticationProvider;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
-                                .anyRequest().permitAll()
+                                .anyRequest().authenticated()
                 )
                 .httpBasic(Customizer.withDefaults());
+
+        http.authenticationProvider(authenticationProvider);
 
         return http.build();
     }
