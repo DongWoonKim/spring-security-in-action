185e012417770b597011637bbd1ebff8d799b59d
DongWoonKim <01030985843z@naver.com>
2024-07-29 13:13:42 +0900

    맞춤형 인증 논리 적용

--- src/main/java/com/example/spring/security/config/SecurityConfig.java
+++ src/main/java/com/example/spring/security/config/SecurityConfig.java
@@ -3,18 +3,35 @@ package com.example.spring.security.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 import javax.sql.DataSource;
+import java.util.HashMap;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
 
 //    private final CustomAuthenticationProvider authenticationProvider;
 
@@ -39,24 +56,30 @@ public class SecurityConfig {
 //        return new InMemoryUserDetailsService(users);
 //    }
 
-    @Bean
-    public UserDetailsService userDetailsService(DataSource dataSource) {
+//    @Bean
+//    public UserDetailsService userDetailsService(DataSource dataSource) {
+//
+//        String usersByUsernameQuery = "select username, password, enabled from spring_security.users where username = ?";
+//        String authsByUserQuery = "select username, authority from spring_security.authorities where username = ?";
+//
+//        var userDetailsManager = new JdbcUserDetailsManager(dataSource);
+//        userDetailsManager.setUsersByUsernameQuery(usersByUsernameQuery);
+//        userDetailsManager.setAuthoritiesByUsernameQuery(authsByUserQuery);
+//
+//        return userDetailsManager;
+//
+////        return new JdbcUserDetailsManager(dataSource);
+//    }
 
-        String usersByUsernameQuery = "select username, password, enabled from spring_security.users where username = ?";
-        String authsByUserQuery = "select username, authority from spring_security.authorities where username = ?";
-
-        var userDetailsManager = new JdbcUserDetailsManager(dataSource);
-        userDetailsManager.setUsersByUsernameQuery(usersByUsernameQuery);
-        userDetailsManager.setAuthoritiesByUsernameQuery(authsByUserQuery);
-
-        return userDetailsManager;
-
-//        return new JdbcUserDetailsManager(dataSource);
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
-    }
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        HashMap<String, PasswordEncoder> encoders = new HashMap<>();
+//
+//        encoders.put("noop", NoOpPasswordEncoder.getInstance());
+//        encoders.put("bcrypt", new BCryptPasswordEncoder());
+//        encoders.put("scrypt", new SCryptPasswordEncoder(16384, 8, 1, 32, 64));
+//
+//        return new DelegatingPasswordEncoder("bcrypt", encoders);
+//    }
 
 }
